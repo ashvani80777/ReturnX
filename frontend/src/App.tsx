2e88abf509d6ee25cb7a5506b723a14ee1a7df24
ashvani80777 <ashvani.sharma@globallogic.com>
2026-08-01 14:33:07 +0530

    Setup react with typescript, tailwind and shadcn ui

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <Button>
+        ReturnX
+      </Button>
+    </div>
+  );
+}
+
+export default App;
